8a8eb63ec7da20228093834cb41e42d74ad78a8d
laudix <rehemamagoti@gmail.com>
2015-04-27 15:42:42 +0300

    first

--- main.php
+++ main.php
@@ -1,11 +1,11 @@
 <?php
-    echo 'Hello world!';
 
-    require "instagram.php";
+require "instagram.php";
+
+$anchor = $_GET["anchor"];
+$insta = new Instagram($anchor);
+echo json_encode($insta->urls, JSON_PRETTY_PRINT);
 
 
-	$anchor = $_GET["anchor"];
-	$insta = new Instagram($anchor);
-	echo json_encode($insta->urls, JSON_PRETTY_PRINT);
 
 ?>
